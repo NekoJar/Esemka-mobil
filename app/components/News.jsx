200db581061d46e1f33138c3843d2246007ddf81
NekoJar <mzharfanhakim7@gmail.com>
2024-04-07 21:20:00 +0700

    fix: type error

---
+++ app/components/News.jsx
@@ -0,0 +1,43 @@
+"use client";
+import { useScroll } from "framer-motion";
+import { useEffect, useRef } from "react";
+import Lenis from "@studio-freight/lenis";
+import { projects } from "../utils/data";
+import Card from "./card/Card";
+
+export function News() {
+  const container = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: container,
+    offset: ["start start", "end end"],
+  });
+
+  useEffect(() => {
+    const lenis = new Lenis();
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+  });
+
+  return (
+    <div ref={container} className="pt-60">
+      {projects.map((project, i) => {
+        const targetScale = 1 - (projects.length - i) * 0.05;
+        return (
+          <Card
+            key={`p_${i}`}
+            i={i}
+            {...project}
+            progress={scrollYProgress}
+            range={[i * 0.25, 1]}
+            targetScale={targetScale}
+          />
+        );
+      })}
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 "use client";
 import { Hero } from "./components/Hero";
 import { ZoomParallax } from "./components/ZoomParallax";
+import { News } from "./components/News";
 
 export default function Home() {
   return (
     <main className="bg-black">
       <Hero />
       <ZoomParallax />
+      <News />
     </main>
   );
 }
