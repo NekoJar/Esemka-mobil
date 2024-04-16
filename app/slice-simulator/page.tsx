a61b3fa6cd5d84083323236d38ae38f071721f05
NekoJar <mzharfanhakim7@gmail.com>
2024-04-17 00:15:38 +0700

    fix: importing error

--- app/slice-simulator/page.tsx
+++ app/slice-simulator/page.tsx
@@ -5,16 +5,16 @@ import {
 } from "@slicemachine/adapter-next/simulator";
 import { SliceZone } from "@prismicio/react";
 
-import { components } from "../../slices";
+// import { components } from "../../slices";
 
-export default function SliceSimulatorPage({
-  searchParams,
-}: SliceSimulatorParams) {
-  const slices = getSlices(searchParams.state);
+// export default function SliceSimulatorPage({
+//   searchParams,
+// }: SliceSimulatorParams) {
+//   const slices = getSlices(searchParams.state);
 
-  return (
-    <SliceSimulator>
-      <SliceZone slices={slices} components={components} />
-    </SliceSimulator>
-  );
-}
+//   return (
+//     <SliceSimulator>
+//       <SliceZone slices={slices} components={components} />
+//     </SliceSimulator>
+//   );
+// }
